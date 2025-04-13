21661dca2351288f059072bf3849966cbce9c642
MiniLancelot <nhattnm.ilt@gmail.com>
2025-04-13 13:17:02 +0700

    fix: fix loading logic to load devices only in device gallery page

--- src/pages/DeviceGallery.tsx
+++ src/pages/DeviceGallery.tsx
@@ -1,4 +1,8 @@
-import { useLoaderData, useNavigation, useSearchParams } from "react-router-dom";
+import {
+  useLoaderData,
+  useNavigation,
+  useSearchParams,
+} from "react-router-dom";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import DeviceCard from "../components/Devices/DeviceCard";
@@ -6,16 +10,16 @@ import DeviceFilters from "../components/Devices/DeviceFilters";
 import Pagination from "../components/Devices/Pagination";
 import { DeviceBasicInfoProps } from "../types/DeviceBasicInfo";
 
-
-
 const DeviceGallery = () => {
   const devices = useLoaderData() as DeviceBasicInfoProps[];
   const navigation = useNavigation();
   const [searchParams, setSearchParams] = useSearchParams();
-  const [filteredDevices, setFilteredDevices] = useState<DeviceBasicInfoProps[]>([]);
+  const [filteredDevices, setFilteredDevices] = useState<
+    DeviceBasicInfoProps[]
+  >([]);
 
   const currentPage = Number(searchParams.get("page") || 1);
-  const pageSize = 8; // Devices per page
+  const pageSize = 24; // Devices per page
 
   const weaponFilter = searchParams.get("weapon") || "";
   const visionFilter = searchParams.get("vision") || "";
@@ -63,18 +67,20 @@ const DeviceGallery = () => {
     currentPage * pageSize
   );
 
-  const weaponTypes = Array.from(new Set(devices.map((device) => device.weapon)));
+  const weaponTypes = Array.from(
+    new Set(devices.map((device) => device.weapon))
+  );
   const visionTypes = Array.from(
     new Set(devices.map((device) => device.vision).filter(Boolean) as string[])
   );
 
-  if (navigation.state === "loading") {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#ff7b75]"></div>
-      </div>
-    );
-  }
+  // if (navigation.state === "loading") {
+  //   return (
+  //     <div className="flex justify-center items-center h-screen">
+  //       <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#ff7b75]"></div>
+  //     </div>
+  //   );
+  // }
 
   return (
     <>
@@ -95,31 +101,39 @@ const DeviceGallery = () => {
           currentVision={visionFilter}
           onFilterChange={handleFilterChange}
         />
+    
+    {navigation.state !== "idle"  ? (
+  <div className="flex justify-center items-center py-20">
+    <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#ff7b75]"></div>
+  </div>
+) : (
+  <>
+    <motion.div
+      className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-8"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      {paginatedDevices.length > 0 ? (
+        paginatedDevices.map((device, index) => (
+          <DeviceCard key={device.name} device={device} index={index} />
+        ))
+      ) : (
+        <div className="col-span-full text-center py-10 text-gray-500">
+          No characters match the selected filters
+        </div>
+      )}
+    </motion.div>
 
-        <motion.div
-          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-8"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.5 }}
-        >
-          {paginatedDevices.length > 0 ? (
-            paginatedDevices.map((device, index) => (
-              <DeviceCard key={device.name} device={device} index={index} />
-            ))
-          ) : (
-            <div className="col-span-full text-center py-10 text-gray-500">
-              No characters match the selected filters
-            </div>
-          )}
-        </motion.div>
-
-        {filteredDevices.length > pageSize && (
-          <Pagination
-            currentPage={currentPage}
-            totalPages={totalPages}
-            onPageChange={handlePageChange}
-          />
-        )}
+    {filteredDevices.length > pageSize && (
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
+    )}
+  </>
+)}
       </div>
     </>
   );

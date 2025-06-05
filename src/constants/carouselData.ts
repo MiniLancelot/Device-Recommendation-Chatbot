bf7579adc9ed696368ad736bd0847f07cc25579b
MiniLancelot <nhattnm.ilt@gmail.com>
2025-06-05 10:33:45 +0700

    fix: fix device detail when price is null

--- src/constants/carouselData.ts
+++ src/constants/carouselData.ts
@@ -53,7 +53,7 @@ export interface CarouselItem {
         title: "Xiaomi 14 Ultra - Nhiếp ảnh chuyên nghiệp",
         image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?w=1200&h=600&fit=crop",
         thumbnail: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?w=200&h=100&fit=crop",
-        buttonLink: "xiaomi-14-ultra",
+        buttonLink: "xiaomi-14-ultra-12gb-512gb",
       },
     ],
   
